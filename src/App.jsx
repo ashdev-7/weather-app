7865633078e335c15816f211e62f64147e672876
ashdev-7 <yash23tyagi@gmail.com>
2025-01-16 17:56:56 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Weather from './components/Weather'
+
+const App = () => {
+  return (
+   <>
+
+    <div className='app'>
+      <Weather/>  {/*mounting weather component on app component  */}
+
+    </div>
+
+   </>
+  )
+}
+
+export default App
